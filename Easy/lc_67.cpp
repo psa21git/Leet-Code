50270d4faa62ac05079e77334b8946efde4cb5d7
P Sreyanshu Anupam <p.sreyanshuspazz@gmail.com>
2024-10-12 10:56:15 +0530

    Easiest Problem i guess ?

---
+++ Easy/lc_67.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int mySqrt(int x) {
+        return (int) sqrt(x);
+}
+
+int main() {
+    cout << mySqrt(8) << endl;
+    return 0;
+}
