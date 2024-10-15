f7c656ec2401a0d52538530e5eec860ed75fe45a
P Sreyanshu Anupam <p.sreyanshuspazz@gmail.com>
2024-10-15 08:52:19 +0530

    thoda time laga but oK

--- Easy/lc_268.cpp
+++ Easy/lc_268.cpp
@@ -1,18 +1,19 @@
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
- int missingNumber(vector<int>& nums) {
-    int total = nums.size() * (nums.size() + 1) / 2;
-    int sum = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        sum += nums[i];
-    }
-    return total - sum;
+ int missingNumber(vector<int>& nums,int val) {
+    sort(nums.begin(),nums.end());
+    // used to delete all occurances of a particular value
+    nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+   //  vec.erase(std::remove(vec.begin(), vec.end(), valueToRemove), vec.end());
+   return nums.size();
+
  }
  int main(){
-    vector<int> nums = {3,0,1};
-    cout << missingNumber(nums) << endl;
+    vector<int> nums = {3,2,2,3};
+    int val = 3;
+    cout << missingNumber(nums,val) << endl;
     return 0;
  }
